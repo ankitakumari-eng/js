2b03d3067573911e97a0c8117754d32b65b5a238
Ankita Kumari <ankita.kumari002007@gmail.com>
2026-06-01 10:23:50 +0000

    added variable file

---
+++ 01_basics/variables.js
@@ -0,0 +1,49 @@
+// console.log("Gita")
+// console.log("Gita");
+
+// // let a = 5
+// let b = 10
+// [1,2,3].forEach(n=> console.log(n))
+
+
+
+// let a = 5
+// let b = 10
+// [1,2,3].forEach(n=> console.log(n))
+
+// let num = 10;
+// (function(){
+//     console.log("HELLO");
+// })();
+
+
+
+//variables
+const accountId =14343
+console.log(accountId);
+let accountEmail="gita@gita.com"
+var accountPassword = "1234"
+accountCity ="Jaipur"
+
+// accountId = 2   not allowed
+
+accountEmail ="hc@gmail.com";
+accountPassword="2121";
+accountCity = "Bhubaneshwar";
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+// {
+//     block scope
+// }
+
+/* 
+prefer not to use var beacause of issue in block and functional scope
+use let in ase of var
+without decalraing let and var we can declare variables.This is also a possibility
+*/
+
+
+let accountState;
+console.log(accountState)
+
+//javascript mein agar ham variable lo decalre nahi karengey toh output mein undefined dega
