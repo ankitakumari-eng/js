d3ac83fb824277a2eb09859ee7029973f82af561
Ankita Kumari <ankita.kumari002007@gmail.com>
2026-06-02 05:22:24 +0000

    commited datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+// "use strict"  // treat all js codes as new version 
+
+// alert("HEllo")   //we are using nodejs , not browswer
+
+// alert( 3 + 3)
+
+// console.log(3+3); 
+
+// console.log("Gita");
+
+// let name = "Gita"
+// let age = 18
+// let isloggedIn = false
+let hero;
+
+// number => 2 to power 53
+//bigint
+//string
+//boolean => false/true 
+
+// null=> standalone value 
+//undefined 
+let  state = null
+
+//symbol => unique 
+
+
+//objects
+let age = 18 
+console.log(typeof"Gita");
+console.log(typeof age);
+console.log(typeof null);   //object
+console.log(typeof undefined);    //undefined
