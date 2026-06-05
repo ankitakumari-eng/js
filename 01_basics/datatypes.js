f2abd4f8a72ca8496eea50f66e5e86e0fae5587f
Ankita Kumari <ankita.kumari002007@gmail.com>
2026-06-05 04:45:23 +0000

    added datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,33 +1,48 @@
-// "use strict"  // treat all js codes as new version 
+"use strict";     //treat all js code as newer version/js
 
-// alert("HEllo")   //we are using nodejs , not browswer
+// alert("Hello")
+// alert( 3+3 )      we are using nodejs not browser
 
-// alert( 3 + 3)
+// console.log("Gita");  
+//  console.log("gita")
 
-// console.log(3+3); 
+let name = "Gita"
+let age = 18
+let isLoggedIn = false
 
-// console.log("Gita");
-
-// let name = "Gita"
-// let age = 18
-// let isloggedIn = false
-let hero;
-
-// number => 2 to power 53
+//primitive datatypes
+// string
+//number   2 to power 53
 //bigint
-//string
-//boolean => false/true 
+//boolean => true or false 
+ //null => standalone value   =>representation of empty
+ //undefined  =>  variable declared but not value
 
-// null=> standalone value 
-//undefined 
-let  state = null
+ let state = null
+ console.log(state)
 
-//symbol => unique 
+ //symbol =>unique
 
 
-//objects
-let age = 18 
-console.log(typeof"Gita");
-console.log(typeof age);
-console.log(typeof null);   //object
-console.log(typeof undefined);    //undefined
+ //non-primitive
+ let person = {
+    name: "Gita",
+    age: 18
+ };     //object
+
+
+ let fruits = ["Apple", "Mango","Papaya"]   //arary
+
+function greet ()  {
+    console.log("Hello");
+}    //function
+
+
+console.log( typeof "Gita");
+
+console.log( typeof age);
+
+console.log( typeof null);
+
+console.log( typeof undefined);
+
