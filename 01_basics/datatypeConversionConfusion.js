45c9adf10b0e1505be6a46bc84932946aa481dd0
Ankita Kumari <ankita.kumari002007@gmail.com>
2026-06-17 20:36:43 +0000

    promptSync

---
+++ 01_basics/datatypeConversionConfusion.js
@@ -0,0 +1,29 @@
+// let score = "34abc"
+
+// //  const {score} = req.body   //no gurantee of score of being string, number...
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber )
+// console.log(ValueInNumber )
+
+
+// let score = null
+
+// console.log(typeof score)
+
+// let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber )
+// console.log(ValueInNumber)
+
+
+
+let score = undefined
+
+// console.log(typeof score)
+
+let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber )
+// console.log(ValueInNumber)
